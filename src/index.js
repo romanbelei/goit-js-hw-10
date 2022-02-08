b019c1ac3c610f8fc531e3ed1a449319f22de3a6
romanbelei <45937404+romanbelei@users.noreply.github.com>
2022-02-08 20:45:22 +0200

    Json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './sass/main.scss';
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    inputCantry: document.querySelector('#search-box')
+}
+
+const onInput = refs.inputCantry.addEventListener('click', onInputFind());
+
+function onInputFind() { console.dir(refs.inputCantry) };
